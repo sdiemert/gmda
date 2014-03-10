9192905c52427259ff0b9c63753e653718b3d932
sdiemert <simon.diemert@gmail.com>
2014-03-10 13:20:00 -0700

    added sidebar file

--- index.php
+++ index.php
@@ -15,14 +15,7 @@
                 <h3 class="page-title">G.M. Diemert Architect Inc.</h3>
                 <hr>
                 <div class="row">
-                    <div class="small-4 columns">
-                        <ul class="side-nav">
-                            <li><a href="index.php">Home</a></li>
-                            <li><a href="portfolio.php">Portfolio</a></li>
-                            <li><a href="about.php">About</a></li>
-                            <li><a href="contact.php">Contact</a></li>
-                        </ul>
-                    </div>
+                    <?php include("sidebar.php"); ?>
                 </div>
             </div>
         </div>
---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simondiemert
+ * Date: 2014-03-10
+ * Time: 1:08 PM
+ */?>
+
+<div class="small-4 columns">
+    <ul class="nav-bar vertical">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="portfolio.php">Portfolio</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</div>
